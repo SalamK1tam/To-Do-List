64618734604b53da148b1ea29d783b67f2118103
SalamK1tam <martynenkovad5@gmail.com>
2026-03-04 00:24:47 +0300

    full project w/adaptiveness

---
+++ ToDO.js
@@ -0,0 +1,59 @@
+var myNodeList = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodeList.length; i++ ) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodeList[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+function newElement() {
+    var li = document.createElement('LI');
+    var inputValue = document.getElementById('Input').value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+        if (inputValue === '') {
+            return;
+        }
+        else {
+            document.getElementById("UL").appendChild(li);
+        }
+        document.getElementById("Input").value = "";
+
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        myNodeList[i].appendChild(span);
+
+        for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
+
+var input = document.getElementById("Input");
+input.addEventListener("keydown", function(event){
+    if (event.key === "Enter") {
+        newElement();
+    }
+}) 
